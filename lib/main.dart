296f5dcfd140cac6f72be45204b2ced4e27f91c3
mpark <mpark_intern@insilicogen.com>
2022-11-11 15:04:11 +0900

    [Update] 안드로이드 머터리얼 디자인을 적용한 탭바리스트 어플 개발

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 // // 02-4-1 스트림 통신하기
@@ -233,33 +236,270 @@ import 'package:flutter/material.dart';
 // 05-1 탭바로 화면 이동하기
 import 'package:flutter/material.dart';
 
-class FistApp extends StatelessWidget {
-  const FistApp({super.key});
+class FirstApp extends StatelessWidget {
+  List<Animal>? list;
+  FirstApp({super.key, this.list});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: Center(
-          child: Text('첫 번째 페이지'),
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  AlertDialog dialog = AlertDialog(
+                    content: Text(
+                      '이 동물은 ${list![index].kind}입니다',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  );
+                  showDialog(context: context, builder: ((context) => dialog));
+                },
+                child: Card(
+                  child: Row(children: [
+                    Image.asset(
+                      list![index].imagePath,
+                      height: 100,
+                      width: 100,
+                      fit: BoxFit.contain,
+                    ),
+                    Text(list![index].animalName),
+                  ]),
+                ),
+              );
+            },
+            itemCount: list!.length,
+          ),
         ),
       ),
     );
   }
 }
 
-class SecondApp extends StatelessWidget {
-  const SecondApp({super.key});
+class SecondApp extends StatefulWidget {
+  List<Animal>? list;
+  SecondApp({super.key, this.list});
+
+  @override
+  State<SecondApp> createState() => _SecondAppState();
+}
+
+class _SecondAppState extends State<SecondApp> {
+  final nameController = TextEditingController();
+  int _radioValue = 0;
+  bool flyExist = false;
+  var _imagePath;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: Center(
-          child: Text('두 번째 페이지'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: nameController,
+                keyboardType: TextInputType.text,
+                maxLines: 1,
+              ),
+              Row(
+                children: [
+                  Radio(
+                    value: 0,
+                    groupValue: _radioValue,
+                    onChanged: _radioChange,
+                  ),
+                  Text('양서류'),
+                  Radio(
+                    value: 1,
+                    groupValue: _radioValue,
+                    onChanged: _radioChange,
+                  ),
+                  Text('파충류'),
+                  Radio(
+                    value: 2,
+                    groupValue: _radioValue,
+                    onChanged: _radioChange,
+                  ),
+                  Text('포유류'),
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              ),
+              Row(
+                children: [
+                  Text('날 수 있나요?'),
+                  Checkbox(
+                      value: flyExist,
+                      onChanged: (check) {
+                        setState(() {
+                          flyExist = check ?? false;
+                        });
+                      })
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              ),
+              // Row(
+              //   children: [
+              //     GestureDetector(
+              //       child: Image.asset(
+              //         'repo/images/cow.png',
+              //         width: 80,
+              //       ),
+              //       onTap: () {
+              //         _imagePath = 'repo/images/cow.png';
+              //       },
+              //     ),
+              //     GestureDetector(
+              //       child: Image.asset(
+              //         'repo/images/pig.png',
+              //         width: 80,
+              //       ),
+              //       onTap: () {
+              //         _imagePath = 'repo/images/pig.png';
+              //       },
+              //     ),
+              //     GestureDetector(
+              //       child: Image.asset(
+              //         'repo/images/bee.png',
+              //         width: 80,
+              //       ),
+              //       onTap: () {
+              //         _imagePath = 'repo/images/bee.png';
+              //       },
+              //     ),
+              //     GestureDetector(
+              //       child: Image.asset(
+              //         'repo/images/cat.png',
+              //         width: 80,
+              //       ),
+              //       onTap: () {
+              //         _imagePath = 'repo/images/cat.png';
+              //       },
+              //     ),
+              //     // GestureDetector(
+              //     //   child: Image.asset(
+              //     //     'repo/images/dog.png',
+              //     //     width: 80,
+              //     //   ),
+              //     //   onTap: () {
+              //     //     _imagePath = 'repo/images/dog.png';
+              //     //   },
+              //     // ),
+              //   ],
+              //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+              // ),
+              Container(
+                height: 100,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    GestureDetector(
+                      child: Image.asset(
+                        'repo/images/cow.png',
+                        width: 80,
+                      ),
+                      onTap: () {
+                        _imagePath = 'repo/images/cow.png';
+                      },
+                    ),
+                    GestureDetector(
+                      child: Image.asset(
+                        'repo/images/pig.png',
+                        width: 80,
+                      ),
+                      onTap: () {
+                        _imagePath = 'repo/images/pig.png';
+                      },
+                    ),
+                    GestureDetector(
+                      child: Image.asset(
+                        'repo/images/bee.png',
+                        width: 80,
+                      ),
+                      onTap: () {
+                        _imagePath = 'repo/images/bee.png';
+                      },
+                    ),
+                    GestureDetector(
+                      child: Image.asset(
+                        'repo/images/cat.png',
+                        width: 80,
+                      ),
+                      onTap: () {
+                        _imagePath = 'repo/images/cat.png';
+                      },
+                    ),
+                    GestureDetector(
+                      child: Image.asset(
+                        'repo/images/dog.png',
+                        width: 80,
+                      ),
+                      onTap: () {
+                        _imagePath = 'repo/images/dog.png';
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    var animal = Animal(
+                        imagePath: _imagePath ?? 'repo/images/dog.png',
+                        animalName: nameController.value.text,
+                        kind: getKind(_radioValue),
+                        flyExist: flyExist);
+
+                    AlertDialog dialog = AlertDialog(
+                      title: Text('동물 추가하기'),
+                      content: Text(
+                        '이 동물은 ${animal.animalName} 입니다'
+                        '또 동물의 종류는 ${animal.kind}입니다. \n 이 동물을 추가하시겠습니까?',
+                        style: TextStyle(fontSize: 30),
+                      ),
+                      actions: [
+                        ElevatedButton(
+                            onPressed: () {
+                              widget.list!.add(animal);
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('예')),
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('아니요'))
+                      ],
+                    );
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) => dialog);
+                  },
+                  child: Text('동물 추가하기'))
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  getKind(int radioValue) {
+    switch (radioValue) {
+      case 0:
+        return "양서류";
+      case 1:
+        return "파충류";
+      case 2:
+        return "포유류";
+    }
+  }
+
+  void _radioChange(int? value) {
+    setState(() {
+      _radioValue = value ?? 0;
+    });
   }
 }
 
@@ -273,6 +513,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
   TabController? controller;
+  List<Animal> animalList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -280,7 +522,12 @@ class _MyHomePageState extends State<MyHomePage>
         title: Text('TabBar Example'),
       ),
       body: TabBarView(
-        children: [FistApp(), SecondApp()],
+        children: [
+          FirstApp(
+            list: animalList,
+          ),
+          SecondApp(list: animalList)
+        ],
         controller: controller,
       ),
       bottomNavigationBar: TabBar(
@@ -313,6 +560,23 @@ class _MyHomePageState extends State<MyHomePage>
         print('현재 index, ${controller!.index}');
       }
     });
+
+    animalList.add(
+        Animal(imagePath: 'repo/images/bee.png', animalName: '벌', kind: '곤충'));
+    animalList.add(Animal(
+        imagePath: 'repo/images/cat.png', animalName: '고양이', kind: '포유류'));
+    animalList.add(Animal(
+        imagePath: 'repo/images/cow.png', animalName: '젖소', kind: '포유류'));
+    animalList.add(
+        Animal(imagePath: 'repo/images/dog.png', animalName: '개', kind: '포유류'));
+    animalList.add(Animal(
+        imagePath: 'repo/images/fox.png', animalName: '여우', kind: '포유류'));
+    animalList.add(Animal(
+        imagePath: 'repo/images/monkey.png', animalName: '원숭이', kind: '영장류'));
+    animalList.add(Animal(
+        imagePath: 'repo/images/pig.png', animalName: '돼지', kind: '포유류'));
+    animalList.add(Animal(
+        imagePath: 'repo/images/wolf.png', animalName: '늑대', kind: '포유류'));
   }
 
   @override
@@ -336,5 +600,78 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
       home: MyHomePage(),
     );
+  }
+}
+
+class Animal {
+  String imagePath;
+  String animalName;
+  String kind;
+  bool? flyExist = false;
+  Animal({
+    required this.imagePath,
+    required this.animalName,
+    required this.kind,
+    this.flyExist,
+  });
+
+  Animal copyWith({
+    String? imagePath,
+    String? animalName,
+    String? kind,
+    bool? flyExist,
+  }) {
+    return Animal(
+      imagePath: imagePath ?? this.imagePath,
+      animalName: animalName ?? this.animalName,
+      kind: kind ?? this.kind,
+      flyExist: flyExist ?? this.flyExist,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'imagePath': imagePath,
+      'animalName': animalName,
+      'kind': kind,
+      'flyExist': flyExist,
+    };
+  }
+
+  factory Animal.fromMap(Map<String, dynamic> map) {
+    return Animal(
+      imagePath: map['imagePath'] as String,
+      animalName: map['animalName'] as String,
+      kind: map['kind'] as String,
+      flyExist: map['flyExist'] as bool,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Animal.fromJson(String source) =>
+      Animal.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Animal(imagePath: $imagePath, animalName: $animalName, kind: $kind, flyExist: $flyExist)';
+  }
+
+  @override
+  bool operator ==(covariant Animal other) {
+    if (identical(this, other)) return true;
+
+    return other.imagePath == imagePath &&
+        other.animalName == animalName &&
+        other.kind == kind &&
+        other.flyExist == flyExist;
+  }
+
+  @override
+  int get hashCode {
+    return imagePath.hashCode ^
+        animalName.hashCode ^
+        kind.hashCode ^
+        flyExist.hashCode;
   }
 }
